028e9b1f1f47826ff6c6a94ff5d409088b196f85
kaurb <kaurb@LAPTOP-33CUU63A>
2023-02-20 12:31:50 -0500

    validations

---
+++ CharakAyurvedicHospital/Controllers/DoctorsList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CharakAyurvedicHospital.Controllers
+{
+    public class DoctorsList : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- CharakAyurvedicHospital/Models/DoctorList.cs
+++ CharakAyurvedicHospital/Models/DoctorList.cs
@@ -2,6 +2,8 @@
 {
     public class DoctorList
     {
+        public int id;
+
 
     }
 }
